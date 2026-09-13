fa13551ac0fe859008b3c25310cd1af2161064f3
Hisham Muhammed <ichu786780@gmail.com>
2026-09-13 10:36:27 +0530

    Update website

--- Docs/script.js
+++ Docs/script.js
@@ -1,6 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(link=>{
-  link.addEventListener('click',()=>{
-    const target=document.querySelector(link.getAttribute('href'));
-    if(target) target.scrollIntoView({behavior:'smooth'});
-  });
-});
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const t=document.querySelector(a.getAttribute('href'));if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'})}}));
